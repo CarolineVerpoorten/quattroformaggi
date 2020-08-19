0f7b433c3e30377552790248f700a52ad9078889
Fabian Quoidbach <fab@pop-os.localdomain>
2020-08-19 11:52:00 +0200

    Init Github

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php 
+    get_header();
+?>
+    <body <? body_class(); ?>>
+        
+        <?php
+            wp_body_open();
+        ?>
+    </body>
+
+<?php
+get_footer();
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php get_header(); ?>
+<body>
+	
+	<h1>Coucou !</h1>
+    
+<?php get_footer(); ?>
+</body>
