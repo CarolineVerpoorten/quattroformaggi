79e960dc1394f97cb4fd677467c358664a595904
Nicolas-Crommen <64846370+Nicolas-Crommen@users.noreply.github.com>
2020-08-20 16:54:54 +0200

    Jeudi soir

--- front-page.php
+++ front-page.php
@@ -1,13 +1,5 @@
-<?php 
     get_header();
 ?>
-    <body <? body_class(); ?>>
-        
-        <?php
-            wp_body_open();
-        ?>
-    </body>
 
 <?php
 get_footer();
-?>
---
+++ single_resto.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+	<h1>SINGLE RESTO</h1>
+<?php get_footer(); ?>
